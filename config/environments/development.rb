5905d551328a936d62097388be6638a918d3a3d2
Emilio Bernal <ebernalzubieta@gmail.com>
2023-06-20 19:54:11 -0600

    testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,13 +63,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  ActionMailer::Base.smtp_settings = {
-    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    :password => ENV["SENDGRID_API_KEY"], # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'beseta.dev',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
 end
